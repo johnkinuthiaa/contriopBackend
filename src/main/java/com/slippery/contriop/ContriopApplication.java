d5b8ebb98be1eea805bddac11d53ecc3085f397f
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-01-17 23:28:16 +0300

    initial commit

---
+++ src/main/java/com/slippery/contriop/ContriopApplication.java
@@ -0,0 +1,13 @@
+package com.slippery.contriop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContriopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContriopApplication.class, args);
+    }
+
+}
